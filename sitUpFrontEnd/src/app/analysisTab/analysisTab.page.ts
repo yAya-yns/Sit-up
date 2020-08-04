b490a66ad0ffdf34211172a92760ab5bdfde56f2
nydya <mobai.ning@gmail.com>
2020-08-04 20:16:10 +0100

    change the thickness of pie chart

--- sitUpFrontEnd/src/app/analysisTab/analysisTab.page.ts
+++ sitUpFrontEnd/src/app/analysisTab/analysisTab.page.ts
@@ -34,9 +34,14 @@ export class analysisTabPage implements OnInit{
               "rgba(255, 99, 132, 0.5)",
               "rgba(75, 192, 100, 0.5)"
             ],
-            hoverBackgroundColor: ["#FF6384", "#36A2EB"]
+            hoverBackgroundColor: ["#FF6384", "#36A2EB"],
+            
           }
         ]
+      },
+      options: {
+        percentageInnerCutout: 10,
+        cutoutPercentage: 65
       }
     });
 
