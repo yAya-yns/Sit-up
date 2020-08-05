c15a73a0556ead99aea4c3407cdd1975d1756ba8
nydya <mobai.ning@gmail.com>
2020-08-05 23:38:27 +0100

    fixed error and add new function

---
+++ app/api/tfpose.py
@@ -0,0 +1,5 @@
+
+
+def call_tfpose():
+    status, msg = 200, 'model called successfully'
+    return status, msg
--- app/main.py
+++ app/main.py
@@ -1,11 +1,19 @@
 from flask import (Flask,json, request, Response)
 from flask_cors import CORS, cross_origin
+from api import tfpose
 
 app = Flask(__name__)
 cors = CORS(app)
 
 
 @app.route('/ping')
-@cross_origin
+@cross_origin()
 def ping():
-    return Response(json.dumps({'msg': 'pong'}), 200, mimetype=MIMETYPE['json'])
+    return Response(json.dumps({'msg': 'pong'}), 200)
+
+@app.route('/calltfpose')
+@cross_origin()
+def call_tfpose():
+    status, msg = tfpose.call_tfpose()
+    # place-holder return 
+    return Response(json.dumps({'msg': msg}), status)
