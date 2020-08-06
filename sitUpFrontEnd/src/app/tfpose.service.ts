90ae2e44b48a9853dab3851cbcc65589bd813de5
nydya <mobai.ning@gmail.com>
2020-08-06 19:27:46 +0100

    add service call

--- sitUpFrontEnd/src/app/tfpose.service.ts
+++ sitUpFrontEnd/src/app/tfpose.service.ts
@@ -1,9 +1,22 @@
+import { environment } from './../environments/environment';
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TfposeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  call() {
+    let headers = new HttpHeaders();
+    headers = headers.append('Accept', '*/*');
+
+    return this.http.get(
+      environment.endpoint,
+      {headers}
+    )
+  }
 }
+
